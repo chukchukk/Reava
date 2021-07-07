c50b07f8397d331cee64116144970eb71046f27c
denis <kerityryui@mail.ru>
2021-07-07 16:05:24 +0300

    Initialized spring

---
+++ src/main/java/com/threebrata/krok/KrokApplication.java
@@ -0,0 +1,13 @@
+package com.threebrata.krok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threebrata/krok/KrokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threebrata.krok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
